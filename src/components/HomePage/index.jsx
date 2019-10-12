c38c35b0f0a0685b7bdd8686a695e9b3399fdb58
bentrinh <ben@vinova.com.sg>
2019-10-12 15:23:35 +0700

    [BASE] - Demo for state using class object state

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -3,11 +3,25 @@ import { Link } from 'react-router-dom';
 import Navbar from '../shared/NavBar';
 
 export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Dung Trinh"
+        }
+    }
+
+    demo(value) {
+        this.setState({
+            name: value
+        })
+    }
+
     render() {
         return (
             <React.Fragment>
                 <Navbar />
-                <p className="text-danger">Demo</p>
+                <p className="text-danger">{this.state.name}</p>
+                <button onClick={() => this.demo("CNPMNC")}>Demo</button>
                 <Link to="/demo">Rent</Link>
             </React.Fragment>
         )
--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -5,7 +5,7 @@ export default class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link to="#">Navbar</Link>
             </nav>
         )
     }
